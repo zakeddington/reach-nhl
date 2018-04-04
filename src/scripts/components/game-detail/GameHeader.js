26eb59fae231d740d9ed64b2800ad7e433bbf550
Zak Eddington <zak.eddington@wearepop.com>
2018-04-04 14:43:02 -0700

    more mobile styles

--- src/scripts/components/game-detail/GameHeader.js
+++ src/scripts/components/game-detail/GameHeader.js
@@ -13,6 +13,13 @@ class GameHeader extends Component {
 	renderContent(data) {
 		return (
 			<header className="game-header">
+				<div className="col game-header-date-info">
+					<span className="game-header-date">{data.date}</span>
+					{
+						data.isPreview &&
+						<span className="game-header-time">{data.gameStatus}</span>
+					}
+				</div>
 				<div className="col game-header-team away">
 					<Logo teamId={data.teams.away.id} />
 					<div className="game-header-team-info">
@@ -25,24 +32,17 @@ class GameHeader extends Component {
 						<div className="game-header-score">{data.teams.away.score}</div>
 					}
 				</div>
-				<div className="col game-header-date-info">
-					<span className="game-header-date">{data.date}</span>
-					{
-						data.isPreview &&
-						<span className="game-header-time">{data.gameStatus}</span>
-					}
-				</div>
 				<div className="col game-header-team home">
-					{
-						!data.isPreview &&
-						<div className="game-header-score">{data.teams.home.score}</div>
-					}
+					<Logo teamId={data.teams.home.id} />
 					<div className="game-header-team-info">
 						<span className="game-header-city">{data.teams.home.city}</span>
 						<span className="game-header-name">{data.teams.home.name}</span>
 						<span className="game-header-record">{data.teams.home.record}</span>
 					</div>
-					<Logo teamId={data.teams.home.id} />
+					{
+						!data.isPreview &&
+						<div className="game-header-score">{data.teams.home.score}</div>
+					}
 				</div>
 			</header>
 		);
